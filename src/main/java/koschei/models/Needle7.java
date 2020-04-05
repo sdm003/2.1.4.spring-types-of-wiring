7bf96a2e129775ab32b7eccbe02cfd3c3fee1308
Daniyar <daniyar743@gmail.com>
2020-04-05 16:53:37 +0600

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private final Deth8 Deth8;
+
+    @Autowired
+    public Needle7(koschei.models.Deth8 deth8) {
+        Deth8 = deth8;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( ";
+    }
+}
